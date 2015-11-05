00b81e75b0d73d38542ffd658beed4fdd6f30051
ngnmhieu <ngnmhieu@gmail.com>
2015-11-06 00:36:07 +0100

    separate server.js into app.js and bootstrap.js

---
+++ app.js
@@ -0,0 +1,35 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express  = require('./config/express');
+
+var db = require('./config/mongodb');
+
+var config = require('./config/config');
+
+/**
+ * Express object
+ */
+var app;
+
+/**
+ * Initializes application 
+ */
+exports.init = function (callback) {
+    
+    db.init(function (err) {
+
+        if (err) throw err;
+
+        var app = express();
+
+        app.listen(config.app.port);
+
+        exports.express = app;
+
+        console.log('[ MODE: ' + process.env.NODE_ENV + ' ] Server running at http://localhost:3000');
+
+        if (typeof callback === 'function')
+            callback();
+    });
+};
+
